b9f341a71f37b32dacdf5a9d705d6986d6717c79
lindembergs <lindembergparaiba@gmail.com>
2023-09-21 22:19:57 -0300

    criação e import do modulo shared

---
+++ src/shared/shared,module.ts
@@ -0,0 +1,24 @@
+import { CommonModule } from "@angular/common";
+import { NgModule } from "@angular/core";
+
+import { HeaderComponent } from "./header/header.component";
+import { FooterComponent } from "./footer/footer.component";
+
+@NgModule({
+  declarations: [
+    HeaderComponent,
+    FooterComponent
+  ],
+  imports: [CommonModule],
+  exports: [
+    HeaderComponent,
+    FooterComponent
+  ],
+  providers: [],
+})
+export class SharedModule {}
+
+
+
+CommonModule
+NgModule
