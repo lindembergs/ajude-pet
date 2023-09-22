5d1088a1ea33ba65d81e06de5933d714e03e165a
lindembergs <lindembergparaiba@gmail.com>
2023-09-21 23:35:31 -0300

    configuração do bootstrap e bindg de propriedade para a logo

--- src/shared/header/header.component.ts
+++ src/shared/header/header.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./header.component.css']
 })
 export class HeaderComponent {
-
+  logoUrl = 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQh0suOJsidzRxm1vu3Du65yc7-A9mGTUlCJA&usqp=CAU';
 }
