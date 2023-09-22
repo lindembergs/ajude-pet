312ae4df623b003a2d49d7e5c4ff568ba097e2e6
lindembergs <lindembergparaiba@gmail.com>
2023-09-22 01:40:41 -0300

    ajuste no background do header

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,17 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { SharedModule } from 'src/shared/shared,module';
+import { HomeComponent } from './pages/home/home.component';
+import { AboutComponent } from './pages/about/about.component';
+import { ContactComponent } from './pages/contact/contact.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    AboutComponent,
+    ContactComponent
   ],
   imports: [
     BrowserModule,
--- src/shared/header/header.component.ts
+++ src/shared/header/header.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./header.component.css']
 })
 export class HeaderComponent {
-  logoUrl = 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQh0suOJsidzRxm1vu3Du65yc7-A9mGTUlCJA&usqp=CAU';
+  logoUrl = 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSmNE8z3LlscYcPaTF9EdHoL9_rnMch8eh22w&usqp=CAU';
 }
