a05a2c09f8f9008afe998498c72035c828826f49
lindembergs <lindembergparaiba@gmail.com>
2023-09-22 23:17:51 -0300

    implementação dos cards de doações

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -7,7 +7,7 @@ import { Component } from '@angular/core';
 })
 export class HomeComponent {
   urlMain = 'https://scontent.fjpa5-1.fna.fbcdn.net/v/t1.6435-9/95656648_1605517829600298_4852976052756021248_n.jpg?stp=dst-jpg_s960x960&_nc_cat=106&ccb=1-7&_nc_sid=e3f864&_nc_eui2=AeGWP9eYlGRTG0C8ZNDQcYaUrWOQcHsr0i2tY5BweyvSLYZsclUhH9QG3kxWy3tiSQKMJTexOS1aLyT6UedbF7pr&_nc_ohc=GT6YTfg4iisAX-xnJPB&_nc_ht=scontent.fjpa5-1.fna&oh=00_AfAauyM3zICZ3Jr2ihKDyujGgPjIhJ6yJQDfDQ7ApaeVsA&oe=65356B60';
-  carrosselImagem1 = 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQS8Zp-OuTjxN9jIvIoLPF5ZXzaCS3IQGWVrQ&usqp=CAU';
-  carrosselImagem2 = 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTX_oXfM1vM1FYvQk9rb2-YN9rTl5yVzKFZunkDUNzaSpJJKzYIZaW0cub4hZn0jfeyVPc&usqp=CAU';
-  carrosselImagem3 = 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRXlPdPtZrNNPaKyrAzjfOHa7qwsZGhXzoHVQ&usqp=CAU';
+  cardImagem1 = 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQS8Zp-OuTjxN9jIvIoLPF5ZXzaCS3IQGWVrQ&usqp=CAU';
+  cardImagem2 = 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTX_oXfM1vM1FYvQk9rb2-YN9rTl5yVzKFZunkDUNzaSpJJKzYIZaW0cub4hZn0jfeyVPc&usqp=CAU';
+  cardImagem3 = 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRXlPdPtZrNNPaKyrAzjfOHa7qwsZGhXzoHVQ&usqp=CAU';
 }
