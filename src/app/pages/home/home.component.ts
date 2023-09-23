815372bea8ac2194d47fc2b927dce7a30714d457
lindembergs <lindembergparaiba@gmail.com>
2023-09-22 22:57:32 -0300

    muita tentativa e erro em fazer o carrossel funcionar da forma correta.

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -7,5 +7,7 @@ import { Component } from '@angular/core';
 })
 export class HomeComponent {
   urlMain = 'https://scontent.fjpa5-1.fna.fbcdn.net/v/t1.6435-9/95656648_1605517829600298_4852976052756021248_n.jpg?stp=dst-jpg_s960x960&_nc_cat=106&ccb=1-7&_nc_sid=e3f864&_nc_eui2=AeGWP9eYlGRTG0C8ZNDQcYaUrWOQcHsr0i2tY5BweyvSLYZsclUhH9QG3kxWy3tiSQKMJTexOS1aLyT6UedbF7pr&_nc_ohc=GT6YTfg4iisAX-xnJPB&_nc_ht=scontent.fjpa5-1.fna&oh=00_AfAauyM3zICZ3Jr2ihKDyujGgPjIhJ6yJQDfDQ7ApaeVsA&oe=65356B60';
-  carrosselImagem1 = 'https://scontent.fjpa5-1.fna.fbcdn.net/v/t31.18172-8/29744701_978956085589812_3456624350976427418_o.jpg?stp=c8.0.206.206a_dst-jpg_p206x206&_nc_cat=108&ccb=1-7&_nc_sid=da31f3&_nc_eui2=AeE-X1QE4eqCuqN6MfK7xwxE4QMoBxN41NrhAygHE3jU2mwshV-IRwJxhjDkj2BsvQfaB29ZaxVGR8OUURF5c_iv&_nc_ohc=U0U43RrinkEAX-TQVsy&_nc_ht=scontent.fjpa5-1.fna&oh=00_AfD4G22dfp9sBPz8gwauvx-2xiJrSM7IRWds77WcDjaUOA&oe=653590E9';
+  carrosselImagem1 = 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQS8Zp-OuTjxN9jIvIoLPF5ZXzaCS3IQGWVrQ&usqp=CAU';
+  carrosselImagem2 = 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTX_oXfM1vM1FYvQk9rb2-YN9rTl5yVzKFZunkDUNzaSpJJKzYIZaW0cub4hZn0jfeyVPc&usqp=CAU';
+  carrosselImagem3 = 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRXlPdPtZrNNPaKyrAzjfOHa7qwsZGhXzoHVQ&usqp=CAU';
 }
