c0187542ab77c6f00d71f71be7b768bbaf80ca5d
lindembergs <lindembergparaiba@gmail.com>
2023-09-23 21:26:06 -0300

    ajuste na imagem da sessão sobre nós

--- src/app/pages/about/about.component.ts
+++ src/app/pages/about/about.component.ts
@@ -6,5 +6,6 @@ import { Component } from '@angular/core';
   styleUrls: ['./about.component.css']
 })
 export class AboutComponent {
+  urlImg = 'https://scontent.fjpa5-1.fna.fbcdn.net/v/t31.18172-8/19250832_506611409670355_7491805246578701528_o.jpg?_nc_cat=104&ccb=1-7&_nc_sid=174925&_nc_eui2=AeEvDWUvJ84mhPTKcHCDNzxy9ozImVZLiAD2jMiZVkuIADHoYlF7h_u0cOznKvwDkkVgPKU3P3khnFXF04-pcRlr&_nc_ohc=mCBVefqL5nEAX-EX6fM&_nc_ht=scontent.fjpa5-1.fna&oh=00_AfBDVXFvR9GHTrY4XX8lAsqMKkXmO-EXbcqt3hGehA9j8w&oe=6536DF45';
 
 }
