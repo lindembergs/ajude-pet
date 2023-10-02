ef7b3254497faaf234230de56f2ec604b917395f
lindembergs <lindembergparaiba@gmail.com>
2023-10-01 23:16:43 -0300

    implementação do @media e teste de adição de novos cards e modal

--- src/app/pages/adopt/adopt.component.ts
+++ src/app/pages/adopt/adopt.component.ts
@@ -6,6 +6,6 @@ import { Component } from '@angular/core';
   styleUrls: ['./adopt.component.css']
 })
 export class AdoptComponent {
-  imgDogs = 'https://cdn.pixabay.com/photo/2018/10/01/09/21/pets-3715733_640.jpg';
-
+  imgCard1 = 'https://scontent.fjpa5-1.fna.fbcdn.net/v/t39.30808-6/307647887_2338570886294985_5693269641265924658_n.jpg?_nc_cat=105&ccb=1-7&_nc_sid=49d041&_nc_ohc=S-ewLDAPAE8AX9YUfje&_nc_ht=scontent.fjpa5-1.fna&oh=00_AfBqP7h-IDStMrAeGElXUmRzy_mzVwKKqXjQPzi4272gSw&oe=651EFA2E';
+  imgCard2 = 'https://scontent.fjpa5-1.fna.fbcdn.net/v/t39.30808-6/286411661_2250820161736725_6685663124247440654_n.jpg?_nc_cat=103&ccb=1-7&_nc_sid=49d041&_nc_ohc=I8lMrQJkg84AX_T9jA9&_nc_ht=scontent.fjpa5-1.fna&oh=00_AfDMQnhQIOAvWI3tLcbo8Ddly1srzKPE6-MxLZ8GMyqViA&oe=651FC6B9';
 }
