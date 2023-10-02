5ce021a8f644d43e6375e00af33847ec2dfc090f
lindembergs <lindembergparaiba@gmail.com>
2023-10-01 23:28:00 -0300

    teste de imagens de diferentes tamanhos nos cards

--- src/app/pages/adopt/adopt.component.ts
+++ src/app/pages/adopt/adopt.component.ts
@@ -7,5 +7,5 @@ import { Component } from '@angular/core';
 })
 export class AdoptComponent {
   imgCard1 = 'https://scontent.fjpa5-1.fna.fbcdn.net/v/t39.30808-6/307647887_2338570886294985_5693269641265924658_n.jpg?_nc_cat=105&ccb=1-7&_nc_sid=49d041&_nc_ohc=S-ewLDAPAE8AX9YUfje&_nc_ht=scontent.fjpa5-1.fna&oh=00_AfBqP7h-IDStMrAeGElXUmRzy_mzVwKKqXjQPzi4272gSw&oe=651EFA2E';
-  imgCard2 = 'https://scontent.fjpa5-1.fna.fbcdn.net/v/t39.30808-6/286411661_2250820161736725_6685663124247440654_n.jpg?_nc_cat=103&ccb=1-7&_nc_sid=49d041&_nc_ohc=I8lMrQJkg84AX_T9jA9&_nc_ht=scontent.fjpa5-1.fna&oh=00_AfDMQnhQIOAvWI3tLcbo8Ddly1srzKPE6-MxLZ8GMyqViA&oe=651FC6B9';
+  imgCard2 = 'https://scontent.fjpa5-1.fna.fbcdn.net/v/t39.30808-6/296798445_616712366722423_7003786350345143925_n.jpg?stp=c0.6.206.206a_dst-jpg_p206x206&_nc_cat=108&ccb=1-7&_nc_sid=0df3a7&_nc_ohc=HjWFJmi7wQoAX-FlLPl&_nc_ht=scontent.fjpa5-1.fna&oh=00_AfBPJ922rqEO9RyNKtuCquYwiBb7BHSRsV-LzKgNWR5oow&oe=651EE091';
 }
