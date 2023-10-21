f1d7c8a4cdfa8a46de4e770fb59cec66e04e1dc3
lindembergs <lindembergparaiba@gmail.com>
2023-10-20 22:00:10 -0300

    remoção de arquivos e variaveis não utilizadas

--- src/app/pages/adopt/adopt.component.ts
+++ src/app/pages/adopt/adopt.component.ts
@@ -7,9 +7,6 @@ import { Title } from '@angular/platform-browser';
   styleUrls: ['./adopt.component.css']
 })
 export class AdoptComponent {
-  imgCard1 = 'https://scontent.fjpa5-1.fna.fbcdn.net/v/t39.30808-6/307647887_2338570886294985_5693269641265924658_n.jpg?_nc_cat=105&ccb=1-7&_nc_sid=49d041&_nc_ohc=S-ewLDAPAE8AX9YUfje&_nc_ht=scontent.fjpa5-1.fna&oh=00_AfBqP7h-IDStMrAeGElXUmRzy_mzVwKKqXjQPzi4272gSw&oe=651EFA2E';
-  imgCard2 = 'https://scontent.fjpa5-1.fna.fbcdn.net/v/t39.30808-6/296798445_616712366722423_7003786350345143925_n.jpg?stp=c0.6.206.206a_dst-jpg_p206x206&_nc_cat=108&ccb=1-7&_nc_sid=0df3a7&_nc_ohc=HjWFJmi7wQoAX-FlLPl&_nc_ht=scontent.fjpa5-1.fna&oh=00_AfBPJ922rqEO9RyNKtuCquYwiBb7BHSRsV-LzKgNWR5oow&oe=651EE091';
-
 
   constructor(private titleService: Title) {
 
--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -16,11 +16,4 @@ export class HomeComponent {
   this.titleService.setTitle('Ajude Pet - Página inicial');
 
  }
-
-  urlMain = 'https://scontent.fjpa5-1.fna.fbcdn.net/v/t1.6435-9/95656648_1605517829600298_4852976052756021248_n.jpg?stp=dst-jpg_s960x960&_nc_cat=106&ccb=1-7&_nc_sid=e3f864&_nc_eui2=AeGWP9eYlGRTG0C8ZNDQcYaUrWOQcHsr0i2tY5BweyvSLYZsclUhH9QG3kxWy3tiSQKMJTexOS1aLyT6UedbF7pr&_nc_ohc=GT6YTfg4iisAX-xnJPB&_nc_ht=scontent.fjpa5-1.fna&oh=00_AfAauyM3zICZ3Jr2ihKDyujGgPjIhJ6yJQDfDQ7ApaeVsA&oe=65356B60';
-  cardImagem1 = 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQS8Zp-OuTjxN9jIvIoLPF5ZXzaCS3IQGWVrQ&usqp=CAU';
-  qrcode = 'https://scontent.fjpa5-1.fna.fbcdn.net/v/t39.30808-6/385004332_2099110460420434_5802087777063996757_n.jpg?stp=dst-jpg_p526x296&_nc_cat=104&ccb=1-7&_nc_sid=49d041&_nc_ohc=qucKsLjvSSsAX90PZpV&_nc_ht=scontent.fjpa5-1.fna&oh=00_AfAotuP8Q9QQqGu3z9utljlIpVQRbZlF4wPSZrMUHlk59w&oe=651DBA19';
-  imagemContato = 'https://cdn.pixabay.com/photo/2022/01/29/13/40/smartphone-6977553_640.jpg';
-  imagemDoe = 'https://media.istockphoto.com/id/1303308191/pt/foto/hungry-brown-dog-with-empty-bowl-waiting-for-feeding.webp?b=1&s=612x612&w=0&k=20&c=Zr218p5ACPGaO3Hv9l2sP0zbpOPdspEsnKQ8dZmtkbA=';
-  imagemAdote = 'https://cdn.pixabay.com/photo/2018/09/23/11/04/dog-3697190_640.jpg';
 }
